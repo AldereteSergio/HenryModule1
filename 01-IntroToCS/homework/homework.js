81288cd649a4d8200563d770f77349ab35723394
Sergio Alderete <alderete.sergio.j@gmail.com>
2023-06-25 17:16:50 -0300

    Ejercicios 1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,26 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   console.log(num);
+   var resultado = 0;
+   var exponente = num.length -1;
 
-function DecimalABinario(num) {}
+   for (let i = 0; i < num.length; i++) {
+      resultado = resultado + num[i] * 2 ** exponente;
+      exponente --
+   }
+   return resultado;
+}
+
+function DecimalABinario(num) {
+   var resultado = "";
+
+   while (num !== 0) {
+      resultado = (num % 2) + resultado
+      num = Math.floor(num/2);
+   }
+   return resultado;
+}
 
 module.exports = {
    BinarioADecimal,
